c29c754f40712a27dd342967e3b7b7c2ff1c19cf
Manipreeth <manipreethbolusani@gmail.com>
2023-05-24 13:34:37 +0530

    CoderBlog name converted to Blogosphere

--- src/Components/Forms/Logo.js
+++ src/Components/Forms/Logo.js
@@ -1,20 +1,12 @@
 import React from "react";
-import { HiCode } from "react-icons/hi";
 
 function Logo() {
   return (
     <div className="CoderBlog">
       <div className="d-flex coderBlogLogo">
-        <HiCode className="coderBlogIcon tc-orange" />
-        <h1>
-          <span className="tc-orange">Coder </span>
-          Blog
-        </h1>
+        <h1 className="tc-orange">BlogoSphere</h1>
       </div>
-      <p className="coderBlogtitle">
-        A place for <span className="tc-orange">programmers</span> by a{" "}
-        <span className="tc-orange">programmer.</span>
-      </p>
+      <p className="coderBlogtitle">Ignite Your Passion for Writing</p>
     </div>
   );
 }
