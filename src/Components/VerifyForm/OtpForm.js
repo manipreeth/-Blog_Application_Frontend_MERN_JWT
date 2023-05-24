893a251a6cb15c76b678e7a4b6d5a5677590dfff
Manipreeth <manipreethbolusani@gmail.com>
2023-05-24 15:02:25 +0530

    Created VerifyForm repository and add OTP verification and email validation forms

---
+++ src/Components/VerifyForm/OtpForm.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import React, { useState, useContext } from "react";
+import { Container, Card, Form, Button } from "react-bootstrap";
+
+import { useNavigate } from "react-router";
+import { ParentContext } from "../../App";
+
+const OtpForm = () => {
+  const props = useContext(ParentContext);
+  const [navState, handleNavState] = props.nav;
+
+  const navigate = useNavigate();
+  const [otp, setOtp] = useState("");
+
+  const handleOtpChange = (e) => {
+    const { value } = e.target;
+    // Only allow 4-digit numbers
+
+    if (/^\d{0,4}$/.test(value)) {
+      setOtp(value);
+    }
+  };
+
+  const handleOtpSubmit = async (e) => {
+    e.preventDefault();
+
+    // Handle OTP validation logic here
+    await axios
+      .post("/api/v1/users/verifyotp", { otp: otp })
+      .then((res) => {
+        handleNavState(!navState);
+        navigate("/");
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+      <Card className="p-4">
+        <Card.Body>
+          <h3 className="mb-4">OTP Validation</h3>
+          <Form onSubmit={handleOtpSubmit}>
+            <Form.Group controlId="otpInput">
+              <Form.Label>Enter OTP (4 digits):</Form.Label>
+              <Form.Control
+                type="number"
+                value={otp}
+                onChange={handleOtpChange}
+                maxLength={4}
+                minLength={4}
+                required
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="w-100 mt-4">
+              Validate OTP
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
+
+export default OtpForm;
---
+++ src/Components/VerifyForm/VerifyAccount.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+import React, { useState, useContext } from "react";
+import { Container, Card, Form, Button } from "react-bootstrap";
+
+import { useLocation, useNavigate } from "react-router";
+
+const VerifyAccount = () => {
+  const navigate = useNavigate();
+  const [otp, setOtp] = useState("");
+
+  const search = useLocation().search;
+  const userId = new URLSearchParams(search).get("userid");
+  console.log(userId);
+
+  const handleOtpChange = (e) => {
+    const { value } = e.target;
+    // Only allow 4-digit numbers
+
+    if (/^\d{0,4}$/.test(value)) {
+      setOtp(value);
+    }
+  };
+
+  const handleOtpSubmit = async (e) => {
+    e.preventDefault();
+
+    // Send JSON Web Token which is stored in LocalStorage for Authorization
+    const token = localStorage.getItem("token");
+
+    // Handle OTP validation logic here
+    await axios
+      .post(
+        `/api/v1/users/verifyEmail/${userId}`,
+        { otp: otp },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        navigate("/login");
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+      <Card className="p-4">
+        <Card.Body>
+          <h3 className="mb-4">OTP Validation</h3>
+          <Form onSubmit={handleOtpSubmit}>
+            <Form.Group controlId="otpInput">
+              <Form.Label>Enter OTP (4 digits):</Form.Label>
+              <Form.Control
+                type="number"
+                value={otp}
+                onChange={handleOtpChange}
+                maxLength={4}
+                minLength={4}
+                required
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="w-100 mt-4">
+              Validate OTP
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
+
+export default VerifyAccount;
