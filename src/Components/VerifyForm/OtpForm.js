183d999a545a61564ca4f4d003cfd7603a9fa8d4
Manipreeth <manipreethbolusani@gmail.com>
2023-05-27 02:19:38 +0530

    Saving token in localStorage

--- src/Components/VerifyForm/OtpForm.js
+++ src/Components/VerifyForm/OtpForm.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Container, Card, Form, Button } from "react-bootstrap";
 
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import { ParentContext } from "../../App";
 
 const OtpForm = () => {
@@ -11,6 +11,9 @@ const OtpForm = () => {
 
   const navigate = useNavigate();
   const [otp, setOtp] = useState("");
+
+  const search = useLocation().search;
+  const userId = new URLSearchParams(search).get("userid");
 
   const handleOtpChange = (e) => {
     const { value } = e.target;
@@ -24,23 +27,18 @@ const OtpForm = () => {
   const handleOtpSubmit = async (e) => {
     e.preventDefault();
 
-    // Send JSON Web Token which is stored in LocalStorage for Authorization
-    const token = localStorage.getItem("token");
-
     // Handle OTP validation logic here
     await axios
-      .post(
-        "/users/verifyotp",
-        { otp: otp },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      )
+      .post(`/users/verifyotp/${userId}`, { otp: otp })
       .then((res) => {
-        handleNavState(!navState);
-        navigate("/");
+        if (res.data.token) {
+          localStorage.setItem("token", res.data.token);
+          handleNavState(!navState);
+          navigate("/");
+        } else {
+          alert("Something Went Wrong");
+          navigate("/");
+        }
       })
       .catch((err) => alert(err.response.data.message));
   };
@@ -49,7 +47,7 @@ const OtpForm = () => {
     <Container className="d-flex justify-content-center align-items-center my-5">
       <Card className="p-4">
         <Card.Body>
-          <h3 className="mb-4">OTP Validation</h3>
+          <h3 className="mb-4">Login OTP Validation</h3>
           <Form onSubmit={handleOtpSubmit}>
             <Form.Group controlId="otpInput">
               <Form.Label>Enter OTP (4 digits):</Form.Label>
